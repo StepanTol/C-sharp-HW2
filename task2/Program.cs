7691e1aeee7e9344ace120994170d1f5356bc984
Stepan <tstepa037@gmail.com>
2022-12-25 00:19:54 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!); 
+if (a<100)
+{
+    Console.WriteLine("Нет третьей цифры");
+}
+else
+{
+    while(a>=1000)
+    {
+    a = a/10;    
+    }
+    int b = a%10;
+    Console.WriteLine($"Третья цифра числа: " + b);
+}
