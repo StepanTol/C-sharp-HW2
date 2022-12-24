52c1353c240f9af85626dc9ae7198de77abacf0f
Stepan <tstepa037@gmail.com>
2022-12-25 00:20:12 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+if (a<6)
+{
+    Console.WriteLine("Нет");
+}
+else
+{
+    if (a>5 & a<8)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Неверная цифра");
+    }    
+}
